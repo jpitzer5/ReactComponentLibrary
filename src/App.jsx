2cc4247a05961549038b80b7a7d77c27cb58d559
Joel Pitzer <jpitzer5@gmail.com>
2025-01-28 20:51:14 -0800

    added size property for button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Button from "./components/button/Button"
+
 export default function App() {
   return (
-    <h1>Hello There</h1>
+    <>
+      <h1>React Component Library</h1>
+      <h2>Buttons</h2>
+      <p>Can use the class names "sm" and "lg" to get smaller/larger buttons</p>
+      <Button size="sm" >Small</Button>
+      <Button >Normal</Button>
+      <Button size="lg">Large</Button>
+    </>
   )
 }
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+import './button.css'
+import clsx from 'clsx'
+
+export default function Button({children, size, className, ...rest}) {
+
+    const classes = clsx(className !== undefined && className, size && `button-${size}`)
+
+    return (
+        <button className={classes} {...rest}>{children}</button>
+    )
+}
